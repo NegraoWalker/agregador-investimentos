dd3be08318f4bbfb6d2b64eb019cddb23a385a7c
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-02-04 07:31:31 -0300

    Implementado listar usuário por id, listar todos os usuários e deleção de usuário

--- src/main/java/com/walker/agregadorinvestimentos/controller/UserController.java
+++ src/main/java/com/walker/agregadorinvestimentos/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -26,12 +27,25 @@ public class UserController {
 
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable("userId") String userId){
-        //...
-        return null;
+        var user = userService.getUserById(userId);
+        if (user.isPresent()){
+            return ResponseEntity.ok(user.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers(){
+        var users = userService.listUsers();
+        return ResponseEntity.ok(users);
+    }
 
-
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteById(@PathVariable("userId") String userId){
+        userService.deleteById(userId);
+        return ResponseEntity.noContent().build();
+    }
 
 
 
--- src/main/java/com/walker/agregadorinvestimentos/service/UserService.java
+++ src/main/java/com/walker/agregadorinvestimentos/service/UserService.java
@@ -6,6 +6,8 @@ import com.walker.agregadorinvestimentos.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -32,8 +34,20 @@ public class UserService {
         return userSaved.getUserId();
     }
 
+    public Optional<User> getUserById(String userId){
+        return userRepository.findById(UUID.fromString(userId));
+    }
 
+    public List<User> listUsers(){
+        return userRepository.findAll();
+    }
 
-
+    public void deleteById(String userId){
+        var id = UUID.fromString(userId);
+        var userExists = userRepository.existsById(id);
+        if (userExists){
+            userRepository.deleteById(id);
+        }
+    }
 
 }
